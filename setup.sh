fd8eaaccd69bca9643d663180c474641390b75df
An Nguyen <shennguyen.redshinobi96@gmail.com>
2022-05-29 14:41:12 +0300

    feat: install vim-plug on new system

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@
 git submodule init
 git submodule update
 
+# Install vim-plug on new system
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # Directories to installed on machine
 directories=(
 	.config
