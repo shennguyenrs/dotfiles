fede50b382fa09aea2b57a6c286de1db0856b2f1
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2023-08-02 09:35:16 +0300

    feat: add new packages & npm install

--- install-packages.sh
+++ install-packages.sh
@@ -4,10 +4,10 @@
 sudo pacman -S \
   stow btop bat mpv exa ranger highlight kitty starship dolphin-plugins flameshot rclone zsh kdeplasma-addons \
   vivaldi vivaldi-ffmpeg-codecs \
-  neovim fzf fd ripgrep nodejs npm luarocks xclip python-neovim \
+  neovim fzf fd ripgrep nodejs npm luarocks xclip python-pyvim \
   lazygit docker docker-compose \
-  ttf-firacode-nerd noto-fonts-cjk noto-fonts-emoji \
-  ktorrent \
+  ttf-firacode-nerd noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd \
+  ktorrent kvantum-qt5 \
   yt-dlp \
 
 # Install yay packages
@@ -16,6 +16,9 @@ yay -S \
   postman-bin visual-studio-code-bin \
   nvim-packer-git \
   lazydocker nvm \
-  ttf-apple-fonts \
+  apple-fonts \
   ibus-bamboo-git ibus-autostart \
   latte-dock-git plasma5-applets-virtual-desktop-bar-git \
+
+# Install NPM packages
+sudo npm install -g neovim pnpm
