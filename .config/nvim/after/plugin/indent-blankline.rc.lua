2ab0e0a9506a92ed5bba140daa37beece098ae2a
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2023-10-17 13:45:54 +0300

    feat: upgrade indent blankline to v3

--- .config/nvim/after/plugin/indent-blankline.rc.lua
+++ .config/nvim/after/plugin/indent-blankline.rc.lua
@@ -1,12 +1,20 @@
-local status_ok, indent_blankline = pcall(require, 'indent_blankline')
+local status_ok, indent_blankline = pcall(require, 'ibl')
 if not status_ok then return end
 
 indent_blankline.setup {
   -- char = "▏",
-  char = "│",
-  filetype_exclude = {
-    "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy"
-  },
-  show_trailing_blankline_indent = false,
-  show_current_context = false
+  -- char = "│",
+  -- filetype_exclude = {
+  --   "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy"
+  -- }
+  -- show_trailing_blankline_indent = false,
+  -- show_current_context = false
+  indent = { char = "┊" },
+  scope = { show_start = false }
 }
+
+local hooks = require "ibl.hooks"
+hooks.register(hooks.type.WHITESPACE,
+  hooks.builtin.hide_first_space_indent_level)
+
+hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
