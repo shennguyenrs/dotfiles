97aa8bef3295cf01fc511193bf8074c06d1cb6f0
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2024-03-08 15:09:52 +0200

    feat: move eslint_d to none-ls extras

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,4 +1,4 @@
--- Install some source from Manson to support this Null ls
+-- Install some source from Manson to support for linting and formatting
 -- prettierd
 -- eslint_d
 -- stylua
@@ -10,40 +10,44 @@
 -- yapf
 
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        local builtins = null_ls.builtins
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local builtins = null_ls.builtins
 
-        local sources = {
-            builtins.formatting.prettierd,
-            builtins.formatting.gofumpt,
-            builtins.diagnostics.sqlfluff.with({ extra_args = { "--dialect", "postgres" } }),
-            builtins.formatting.stylua,
-            builtins.formatting.goimports,
-            builtins.formatting.rustfmt,
-            builtins.diagnostics.eslint_d.with({
-                diagnostics_format = '[eslint] #{m}\n(#{c})'
-            }),
-            builtins.code_actions.gitsigns,
-            builtins.code_actions.refactoring,
-            builtins.formatting.sql_formatter,
-            builtins.formatting.yapf
-        }
+		local sources = {
+			builtins.formatting.prettierd,
+			builtins.formatting.gofumpt,
+			builtins.diagnostics.sqlfluff.with({ extra_args = { "--dialect", "postgres" } }),
+			builtins.formatting.stylua,
+			builtins.formatting.goimports,
+			-- builtins.formatting.rustfmt,
+			-- builtins.diagnostics.eslint_d.with({
+			--   diagnostics_format = "[eslint] #{m}\n(#{c})",
+			-- }),
+			builtins.code_actions.gitsigns,
+			builtins.code_actions.refactoring,
+			builtins.formatting.sql_formatter,
+			builtins.formatting.yapf,
+			require("none-ls.diagnostics.eslint_d"),
+		}
 
-        null_ls.setup({
-            sources = sources,
-            on_attach = function(client, bufnr)
-                if client.server_capabilities.documentFormattingProvider then
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = vim.api.nvim_create_augroup("Format", { clear = true }),
-                        buffer = bufnr,
-                        callback = function()
-                            vim.lsp.buf.format { async = false }
-                        end
-                    })
-                end
-            end
-        })
-    end
+		null_ls.setup({
+			sources = sources,
+			on_attach = function(client, bufnr)
+				if client.server_capabilities.documentFormattingProvider then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = vim.api.nvim_create_augroup("Format", { clear = true }),
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
