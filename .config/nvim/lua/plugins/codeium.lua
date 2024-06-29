7912a557314e08e22a696546203b000c2419e0d6
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2024-06-29 14:30:37 +0300

    fix: update configs
    - Change model of aichat
    - Add chat messages to .gitignore
    - Add aichat to install packages
    - Disabled codeium on json filetype

--- .config/nvim/lua/plugins/codeium.lua
+++ .config/nvim/lua/plugins/codeium.lua
@@ -1,51 +1,52 @@
 return {
-	"Exafunction/codeium.vim",
-	event = "BufEnter",
-	keys = {
-		{
-			"<C-g>",
-			function()
-				return vim.fn["codeium#Accept"]()
-			end,
-			mode = "i",
-			expr = true,
-			silent = true,
-		},
-		{
-			"<C-;>",
-			function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end,
-			mode = "i",
-			expr = true,
-			silent = true,
-		},
-		{
-			"<C-,>",
-			function()
-				return vim.fn["codeium#CycleCompletions"](-1)
-			end,
-			mode = "i",
-			expr = true,
-			silent = true,
-		},
-		{
-			"<C-x>",
-			function()
-				return vim.fn["codeium#Clear"]()
-			end,
-			mode = "i",
-			expr = true,
-			silent = true,
-		},
-	},
-	config = function()
-		vim.g.codeium_disable_bindings = 1
-		vim.cmd([[
+  "Exafunction/codeium.vim",
+  event = "BufEnter",
+  keys = {
+    {
+      "<C-g>",
+      function()
+        return vim.fn["codeium#Accept"]()
+      end,
+      mode = "i",
+      expr = true,
+      silent = true,
+    },
+    {
+      "<C-;>",
+      function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end,
+      mode = "i",
+      expr = true,
+      silent = true,
+    },
+    {
+      "<C-,>",
+      function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end,
+      mode = "i",
+      expr = true,
+      silent = true,
+    },
+    {
+      "<C-x>",
+      function()
+        return vim.fn["codeium#Clear"]()
+      end,
+      mode = "i",
+      expr = true,
+      silent = true,
+    },
+  },
+  config = function()
+    vim.g.codeium_disable_bindings = 1
+    vim.cmd([[
       let g:codeium_filetypes = {
         \ "markdown": v:false,
         \ "zsh": v:false,
+        \ "json": v:false,
       \ }
     ]])
-	end,
+  end,
 }
--- install-packages.sh
+++ install-packages.sh
@@ -2,46 +2,46 @@
 
 # Install packages
 sudo pacman -S \
-	stow btop bat mpv eza lf highlight kitty starship rclone zsh jq \
-	chromium vivaldi vivaldi-ffmpeg-codecs firefox-developer-edition \
-	neovim fzf fd ripgrep nodejs npm luarocks xclip python-pynvim python-pip \
-	lazygit docker docker-compose docker-buildx go \
-	ttf-firacode-nerd noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd \
-	qbittorrent yt-dlp calibre gparted \
-	fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-unikey
+  stow btop bat mpv eza lf highlight kitty starship rclone zsh jq \
+  chromium vivaldi vivaldi-ffmpeg-codecs firefox-developer-edition \
+  neovim fzf fd ripgrep nodejs npm luarocks xclip python-pynvim python-pip \
+  lazygit docker docker-compose docker-buildx go \
+  ttf-firacode-nerd noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd \
+  qbittorrent yt-dlp calibre gparted \
+  fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-unikey
 
 # Install yay packages
 yay -S \
-	rxfetch \
-	telegram-desktop-bin \
-	postman-bin visual-studio-code-bin bun-bin \
-	lazydocker-bin nvm \
-	apple-fonts \
-	microsoft-edge-dev-bin
+  rxfetch aichat \
+  telegram-desktop-bin \
+  postman-bin visual-studio-code-bin bun-bin cursor-appimage \
+  lazydocker-bin nvm \
+  apple-fonts \
+  microsoft-edge-dev-bin
 
 # Install NPM packages
 sudo npm install -g neovim pnpm
 
 # KDE
 while true; do
-	read -p "Do you want to install KDE packages? (y/n) " answer
+  read -p "Do you want to install KDE packages? (y/n) " answer
 
-	case $answer in
-	y | Y)
-		echo "Running the command..."
+  case $answer in
+  y | Y)
+    echo "Running the command..."
 
-		sudo pacman -S dolphin-plugins kdeplasma-addons
+    sudo pacman -S dolphin-plugins kdeplasma-addons
 
-		echo "Finish installing packages"
+    echo "Finish installing packages"
 
-		exit 0 # Exit with success code
-		;;
-	n | N)
-		echo "Finish installing packages"
-		exit 0 # Exit with success code
-		;;
-	*)
-		echo "Invalid input. Please enter 'y' or 'n'."
-		;;
-	esac
+    exit 0 # Exit with success code
+    ;;
+  n | N)
+    echo "Finish installing packages"
+    exit 0 # Exit with success code
+    ;;
+  *)
+    echo "Invalid input. Please enter 'y' or 'n'."
+    ;;
+  esac
 done
