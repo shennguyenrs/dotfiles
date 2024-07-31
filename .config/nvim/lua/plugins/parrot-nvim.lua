f01edbf5ec2275f7f2ef15da5021b568ebb81851
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2024-07-31 14:22:42 +0300

    fix: remove gemini due to region restrict of usage

--- .config/nvim/lua/plugins/parrot-nvim.lua
+++ .config/nvim/lua/plugins/parrot-nvim.lua
@@ -1,20 +1,17 @@
 return {
-	"frankroeder/parrot.nvim",
-	event = "BufEnter",
-	dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
-	config = function()
-		require("parrot").setup({
-			providers = {
-				openai = {
-					api_key = os.getenv("OPENAI_API_KEY"),
-				},
-				anthropic = {
-					api_key = os.getenv("ANTHROPIC_API_KEY"),
-				},
-				gemini = {
-					api_key = os.getenv("GEMINI_API_KEY"),
-				},
-			},
-		})
-	end,
+  "frankroeder/parrot.nvim",
+  event = "BufEnter",
+  dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
+  config = function()
+    require("parrot").setup({
+      providers = {
+        openai = {
+          api_key = os.getenv("OPENAI_API_KEY"),
+        },
+        anthropic = {
+          api_key = os.getenv("ANTHROPIC_API_KEY"),
+        },
+      },
+    })
+  end,
 }
