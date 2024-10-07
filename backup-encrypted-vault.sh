e8eddfc4968f9490507d1653992ac8db6dc2d3c5
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2024-10-07 16:24:37 +0300

    chore: update name for clone and backup scripts with rclone

---
+++ backup-encrypted-vault.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/usr/bin/rclone copy \
+  --update \
+  --verbose \
+  --transfers 30 \
+  --checkers 8 \
+  --contimeout 60s \
+  --timeout 300s \
+  --retries 3 \
+  --low-level-retries 10 \
+  --stats 1s \
+  "$HOME/encrypted-vault" "gdrive:encrypted-vault-backup"
