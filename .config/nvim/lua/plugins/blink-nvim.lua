3ec08897652a3e39793cfb7b9f0eac0bc3e3667d
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2026-05-27 11:23:39 +0300

    fix: update blink.nvim config

--- .config/nvim/lua/plugins/blink-nvim.lua
+++ .config/nvim/lua/plugins/blink-nvim.lua
@@ -5,5 +5,12 @@ return {
 
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
-	opts = {},
+	opts = {
+		completion = {
+			documentation = { auto_show = true, auto_show_delay_ms = 500 },
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+	},
 }
