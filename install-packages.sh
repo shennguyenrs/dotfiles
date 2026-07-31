05e2697f7f90c5c8d635415d3c0be313c8a8f1d9
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2026-07-31 11:23:28 +0300

    fix: update install packages script

--- install-packages.sh
+++ install-packages.sh
@@ -2,45 +2,48 @@
 
 # Install packages
 sudo pacman -S \
-	stow btop bat mpv lua luarocks yazi eza highlight kitty starship rclone zsh jq zoxide \
-	chromium vivaldi vivaldi-ffmpeg-codecs firefox-developer-edition \
-	neovim fzf fd ripgrep nodejs npm luarocks xclip python-pynvim python-pip \
-	lazygit docker docker-compose docker-buildx go rustup \
-	ttf-firacode-nerd noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd \
-	qbittorrent yt-dlp gparted \
-	fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-unikey uv bun
+  stow btop bat mpv lua luarocks yazi eza highlight kitty starship zsh jq zoxide python-pynvim \
+  chromium vivaldi vivaldi-ffmpeg-codecs neovim fzf fd ripgrep nodejs npm xclip lazygit \
+  ttf-firacode-nerd noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd qbittorrent yt-dlp \
+  fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-unikey uv bun nvm yay
+# rclone firefox-developer-edition docker docker-compose docker-buildx go rustup gparted
 
 # Install yay packages
 yay -S \
-	rxfetch visual-studio-code-bin microsoft-edge-stable-bin \
-	lazydocker-bin nvm zed-preview-bin apple-fonts
+  antigravity-cli
+#  apple-fonts \
+#  visual-studio-code-bin \
+# microsoft-edge-stable-bin
+# zed-preview-bin
+# lazydocker-bin
 
 # Install NPM packages
-sudo npm install -g neovim pnpm vercel
+sudo npm install -g neovim
+# pnpm vercel
 
 # Remove some default packages
-sudo pacman -Rns firefox
+# sudo pacman -Rns firefox
 
 # KDE
 while true; do
-	read -p "Do you want to install KDE packages? (y/n) " answer
+  read -p "Do you want to install KDE packages? (y/n) " answer
 
-	case $answer in
-	y | Y)
-		echo "Running the command..."
+  case $answer in
+  y | Y)
+    echo "Running the command..."
 
-		sudo pacman -S dolphin-plugins kdeplasma-addons
+    sudo pacman -S dolphin-plugins kdeplasma-addons
 
-		echo "Finish installing packages"
+    echo "Finish installing packages"
 
-		exit 0 # Exit with success code
-		;;
-	n | N)
-		echo "Finish installing packages"
-		exit 0 # Exit with success code
-		;;
-	*)
-		echo "Invalid input. Please enter 'y' or 'n'."
-		;;
-	esac
+    exit 0 # Exit with success code
+    ;;
+  n | N)
+    echo "Finish installing packages"
+    exit 0 # Exit with success code
+    ;;
+  *)
+    echo "Invalid input. Please enter 'y' or 'n'."
+    ;;
+  esac
 done
